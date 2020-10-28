a49224c2599021437610f9bafecea67e37cb4aa6
Banson Xu <xzcxzcyy@126.com>
2020-10-28 21:39:54 +0800

    feat: select_video_fragment

---
+++ app/src/main/java/cloud/banson/xplayer/ui/select/SelectVideoFragment.kt
@@ -0,0 +1,42 @@
+package cloud.banson.xplayer.ui.select
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import cloud.banson.xplayer.R
+import cloud.banson.xplayer.databinding.SelectVideoFragmentBinding
+
+class SelectVideoFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SelectVideoFragment()
+    }
+
+    private lateinit var viewModel: SelectVideoViewModel
+    private lateinit var binding: SelectVideoFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        initView(inflater, container)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(SelectVideoViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+    private fun initView(inflater: LayoutInflater, container: ViewGroup?) {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.select_video_fragment, container, false)
+        
+    }
+
+}
---
+++ app/src/main/java/cloud/banson/xplayer/ui/select/SelectVideoViewModel.kt
@@ -0,0 +1,7 @@
+package cloud.banson.xplayer.ui.select
+
+import androidx.lifecycle.ViewModel
+
+class SelectVideoViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
