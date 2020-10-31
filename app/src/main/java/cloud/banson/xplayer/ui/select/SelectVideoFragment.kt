89eb09997cf9fdc191a05f0ea199b83a862f7f78
Banson Xu <xzcxzcyy@126.com>
2020-10-31 15:59:43 +0800

    ref: move db operations into view model

--- app/src/main/java/cloud/banson/xplayer/ui/select/SelectVideoFragment.kt
+++ app/src/main/java/cloud/banson/xplayer/ui/select/SelectVideoFragment.kt
@@ -1,32 +1,21 @@
 package cloud.banson.xplayer.ui.select
 
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.net.toUri
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.observe
 import androidx.recyclerview.widget.GridLayoutManager
 import cloud.banson.xplayer.R
-import cloud.banson.xplayer.data.Video
-import cloud.banson.xplayer.data.VideoDatabase
 import cloud.banson.xplayer.databinding.SelectVideoFragmentBinding
-import kotlinx.coroutines.*
-import java.io.File
-import java.io.FileOutputStream
 
 class SelectVideoFragment : Fragment() {
 
     companion object {
         private const val TAG = "SelectVideoFragment"
     }
-
-    private val job = Job()
-    private val uiScope = CoroutineScope(Dispatchers.Main + job)
 
     private lateinit var viewModel: SelectVideoViewModel
     private lateinit var binding: SelectVideoFragmentBinding
@@ -36,8 +25,8 @@ class SelectVideoFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        initViewModel()
         initView(inflater, container)
-        initData()
         return binding.root
     }
 
@@ -48,45 +37,20 @@ class SelectVideoFragment : Fragment() {
     private fun initView(inflater: LayoutInflater, container: ViewGroup?) {
         binding =
             DataBindingUtil.inflate(inflater, R.layout.select_video_fragment, container, false)
-        val videoDatabaseDao = VideoDatabase.getInstance(context!!).videoDatabaseDao
-        //TODO: CONVERT LISTENER
-        myAdapter = VideoAdapter {
-            uiScope.launch {
-                withContext(Dispatchers.IO) {
-                    val destination = File(context!!.filesDir, it.name)
-                    it.uri.copyTo(destination)
-                    val newItem = Video(0, destination.toUri().path!!, destination.name)
-                    videoDatabaseDao.insert(newItem)
-                }
-            }
-        }
+
+        myAdapter = VideoAdapter(viewModel.onItemClick)
+
         binding.selectRecyclerView.apply {
             layoutManager = GridLayoutManager(context, 3)
             adapter = myAdapter
         }
     }
 
-    private fun initData() {
+    private fun initViewModel() {
         val viewModelFactory = SelectVideoViewModelFactory(activity!!.applicationContext)
         viewModel = ViewModelProvider(this, viewModelFactory).get(SelectVideoViewModel::class.java)
         viewModel.videoListLive.observe(viewLifecycleOwner) {
             myAdapter.submitList(it)
-        }
-    }
-
-    private fun Uri.copyTo(destination: File) {
-        val inChannel = context!!.contentResolver.openInputStream(this)
-        val outChannel = FileOutputStream(destination)
-        try {
-            val buffer = ByteArray(1024)
-            var length: Int
-
-            while (inChannel!!.read(buffer).also { length = it } > 0) {
-                outChannel.write(buffer, 0, length)
-            }
-        } finally {
-            inChannel?.close()
-            outChannel.close()
         }
     }
 
--- app/src/main/java/cloud/banson/xplayer/ui/select/SelectVideoViewModel.kt
+++ app/src/main/java/cloud/banson/xplayer/ui/select/SelectVideoViewModel.kt
@@ -6,20 +6,37 @@ import android.content.Context
 import android.net.Uri
 import android.provider.MediaStore
 import android.util.Size
+import androidx.core.net.toUri
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import cloud.banson.xplayer.data.Video
+import cloud.banson.xplayer.data.VideoDatabase
 import cloud.banson.xplayer.data.VideoInfo
 import kotlinx.coroutines.*
+import java.io.File
+import java.io.FileOutputStream
 
 class SelectVideoViewModel(private val appContext: Context) : ViewModel() {
     private val job = Job()
     private val uiScope = CoroutineScope(Dispatchers.Main + job)
     private val videoList = mutableListOf<VideoInfo>()
+    private val videoDatabaseDao = VideoDatabase.getInstance(appContext).videoDatabaseDao
     val videoListLive = MutableLiveData<MutableList<VideoInfo>>()
 
     init {
         uiScope.launch {
             updateVideoList()
+        }
+    }
+
+    val onItemClick: (VideoInfo) -> Unit = { item ->
+        uiScope.launch {
+            withContext(Dispatchers.IO) {
+                val destination = File(appContext.filesDir, item.name)
+                item.uri.copyTo(destination)
+                val newItem = Video(0, destination.toUri().path!!, destination.name)
+                videoDatabaseDao.insert(newItem)
+            }
         }
     }
 
@@ -73,4 +90,21 @@ class SelectVideoViewModel(private val appContext: Context) : ViewModel() {
             }
         }
     }
+
+    private fun Uri.copyTo(destination: File) {
+        val inChannel = appContext.contentResolver.openInputStream(this)
+        val outChannel = FileOutputStream(destination)
+        try {
+            val buffer = ByteArray(1024)
+            var length: Int
+
+            while (inChannel!!.read(buffer).also { length = it } > 0) {
+                outChannel.write(buffer, 0, length)
+            }
+        } finally {
+            inChannel?.close()
+            outChannel.close()
+        }
+    }
+
 }
